bdd22808d68c3bc0fadb6c30cc45de29e0ab0e9b
ftesolution <98545736+ftesolution@users.noreply.github.com>
2023-11-23 03:10:10 +0500

    this is inital comment and start of calculator

---
+++ lib/Splash_Scrren.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Splash_Screen extends StatelessWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("this is splash screen of Dart"),
+      ),
+    );
+  }
+}
